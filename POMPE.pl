79a6db08ca4eeea6b458c18c08aa26b08483d5c6
hubert Itomba <Hubert Itomba>
2024-05-03 17:47:45 +0200

    implement initial and folder structure

---
+++ POMPE.pl
@@ -0,0 +1,15 @@
+[hubert@deyvon jsm_banking]$ npx shadcn-ui@latest init
+Need to install the following packages:
+shadcn-ui@0.8.0
+Ok to proceed? (y) y
+✔ Which style would you like to use? › Default
+✔ Which color would you like to use as base color? › Slate
+✔ Would you like to use CSS variables for colors? … no / yes
+
+✔ Writing components.json...
+✔ Initializing project...
+✔ Installing dependencies...
+
+Success! Project initialization completed. You may now add components.
+
+o
